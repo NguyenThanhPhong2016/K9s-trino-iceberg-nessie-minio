0853588ef0731ceb0d1fd913088a6567e4f26ac3
NguyenThanhPhong2016 <phongnt192016@gmail.com>
2025-02-04 15:33:50 +0700

    update dagdag

--- DAG_Airflow/test1.py
+++ DAG_Airflow/test1.py
@@ -5,35 +5,35 @@ from airflow.operators.bash_operator import BashOperator
 from minio import Minio
 from minio.sse import SseCustomerKey
 from trino.dbapi import connect
-def my_task():
-    client = Minio(
-        "192.168.1.17:30090",  # Địa chỉ API MinIO (cổng 9000)
-        access_key="zAibg3ryk50qi0f6oS9T",  # Tài khoản mặc định
-        secret_key="P0QX1qz9sBvbfhf4bgEtA6JNJJIyPMkS5PTd20pA", # Mật khẩu mặc định
-        secure=False
-    )
+# def my_task():
+#     client = Minio(
+#         "192.168.1.17:30090",  # Địa chỉ API MinIO (cổng 9000)
+#         access_key="zAibg3ryk50qi0f6oS9T",  # Tài khoản mặc định
+#         secret_key="P0QX1qz9sBvbfhf4bgEtA6JNJJIyPMkS5PTd20pA", # Mật khẩu mặc định
+#         secure=False
+#     )
 
-    bucket_name = "test-minio"  # Tên bucket
-    object_name = "trino_data.csv"  # Tên file trên MinIO
-    file_path = "trino_data.csv"  # Đường dẫn file cục bộ
+#     bucket_name = "test-minio"  # Tên bucket
+#     object_name = "trino_data.csv"  # Tên file trên MinIO
+#     file_path = "trino_data.csv"  # Đường dẫn file cục bộ
 
-    # Ghi dữ liệu vào MinIO
-    client.fput_object(bucket_name, object_name, file_path)
-    print(f"File '{file_path}' đã được tải lên bucket '{bucket_name}' dưới tên '{object_name}'.")
+#     # Ghi dữ liệu vào MinIO
+#     client.fput_object(bucket_name, object_name, file_path)
+#     print(f"File '{file_path}' đã được tải lên bucket '{bucket_name}' dưới tên '{object_name}'.")
 
-    x = client.list_buckets()
-    for y in x:
-        print(y)
+#     x = client.list_buckets()
+#     for y in x:
+#         print(y)
 with DAG(
     dag_id="test",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,  # Chạy thủ công
     catchup=False,
 ) as dag:
-    task = PythonOperator(
-        task_id="print_message",
-        python_callable=my_task,
-    )
+    # task = PythonOperator(
+    #     task_id="print_message",
+    #     python_callable=my_task,
+    # )
     run_bash_command = BashOperator(
         task_id='run_bash_command',
         bash_command='minio --version'
@@ -42,6 +42,6 @@ with DAG(
         task_id='run_bash_command1',
         bash_command='python --version'
     )
-    task >> run_bash_command >> run_bash_command1
+    run_bash_command >> run_bash_command1
 
     
