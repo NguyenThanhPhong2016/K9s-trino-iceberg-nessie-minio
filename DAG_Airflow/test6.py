07b9e4b3ff8a2116495e37aebdc65293b53c02a6
NguyenThanhPhong2016 <phongnt192016@gmail.com>
2025-02-06 15:21:13 +0700

    them xcom vao test 6

--- DAG_Airflow/test6.py
+++ DAG_Airflow/test6.py
@@ -1,9 +1,18 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+
+
+def process_response(**kwargs):
+    ti = kwargs["ti"]
+    response_data = ti.xcom_pull(task_ids="get_post")  # 🟢 Lấy dữ liệu từ XCom
+    print("📌 Response từ API:", response_data)
+
 
 # Khai báo DAG
 default_args = {"owner": "airflow", "start_date": days_ago(1)}
+
 
 with DAG("http_operator_example", default_args=default_args, schedule_interval=None) as dag:
 
@@ -13,6 +22,12 @@ with DAG("http_operator_example", default_args=default_args, schedule_interval=N
         http_conn_id="http_connect",  # 🟢 Không dùng Connection
         endpoint="",  # 🟢 Truyền URL trực tiếp
         log_response=True,  # 🟢 Ghi log phản hồi từ API
+        do_xcom_push=True,
     )
-    
-    get_post  # ✅ Kích hoạt task
+    process_task = PythonOperator(
+        task_id="process_response",
+        python_callable=process_response,
+        provide_context=True,
+    )
+    get_post >> process_task
+
