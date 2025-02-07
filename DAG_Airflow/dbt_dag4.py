9db497a2037f74b01e9a2fbfd30aa07e579ec5f4
NguyenThanhPhong2016 <phongnt192016@gmail.com>
2025-02-07 18:06:57 +0700

    aaaaaa

--- DAG_Airflow/dbt_dag4.py
+++ DAG_Airflow/dbt_dag4.py
@@ -13,7 +13,7 @@ default_args = {
 
 # Khởi tạo DAG
 dag = DAG(
-    "dbt_kubernetes_dag4",
+    "dbt_kubernetes_dag6",
     default_args=default_args,
     schedule_interval="@daily",  # Chạy DAG mỗi ngày
     catchup=False,
